e7c6d3a6609ea7bc3d166c1cecb45ddf1b8febfa
mahesh0047 <mahesh.tagline@gmail.com>
2020-11-12 15:06:45 +0530

    add new file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def get_city
+     @state = State.find(params[:state_id])
+     @cities = @state.cities
+     p"::::::::::::::#{@cities.class}"
+    # render json: { cities: @cities }
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :addresses
-  get 'companies/index'
   resources :users
   resources :cities
   resources :states
   resources :companies
+  get 'get_city' , to: 'users#get_city' 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
